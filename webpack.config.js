bece10af751d104e8c40a91ac1736e9b6c8641f3
siliconjungle <siliconjungle.io@gmail.com>
2022-03-30 19:44:32 +1100

    update configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,26 @@
-const path = require('path')
+var path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve('lib'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    library: 'index',
+    globalObject: "this",
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom',
   },
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        exclude: path.join(__dirname, '/node_modules/'),
+        loader: 'babel-loader',
       }
-    ],
-  },
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    }
-  },
-  externals: {
-    // Don't bundle react or react-dom
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
-    },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM"
-    }
+    ]
   }
 }
